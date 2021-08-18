9f4a953004749fc722d6fef78415316e4ac3e542
Gleb <Duke9266@mail.ru>
2021-08-18 14:09:54 +0300

    create check database

--- project/db_conection.py
+++ project/db_conection.py
@@ -1,6 +1,9 @@
 import sqlite3
 import os
 from os import path
+import db_init
+
+create_db = db_init.DataBase
 
 
 class DB_Conector():
@@ -11,6 +14,7 @@ class DB_Conector():
         self.connection = None
 
     def create_connection(self):
+        create_db.check_db()
         BASE_DIR = os.path.dirname(os.path.abspath(__file__))
         db_path = os.path.join(BASE_DIR, "world_all.db")
         return sqlite3.connect(db_path)
